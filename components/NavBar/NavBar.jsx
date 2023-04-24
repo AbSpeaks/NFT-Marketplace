0813abb490cdf733dabed99f018d6a881e388164
AbSpeaks <abinbiju0801@gmail.com>
2023-04-24 12:15:00 +0530

    first commit

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,180 @@
+import React ,{useState,useEffect} from "react";
+
+import Image from "next/image";
+import {Button} from "../componentsindex";
+
+import images from "../../img";
+
+//IMPORT ICONS
+
+import { MdCircleNotifications } from "react-icons/md";
+import {BsSearch} from "react-icons/bs";
+import {CgMenuLeft,CgMenuRight} from "react-icons/cg";
+
+
+//INternal import 
+
+import Style from "./NavBar.module.css";
+import {Discover,HelpCenter,Notification,Profile,SideBar} from "./index";
+
+const NavBar =() =>
+{
+    const[discover, setDiscover] = useState(false);
+    const[help, setHelp] = useState(false);
+    const[notification, setNotification] = useState(false);
+    const[profile, setProfile] = useState(false);
+    const[sideMenu, setSideMenu] = useState(false);
+
+    const openMenu =(e) => {
+
+        const btnText = e.target.innerText;
+
+        if(btnText=="Discover")
+        {
+            setDiscover(true);
+            setHelp(false);
+            setNotification(false);
+            setProfile(false);
+
+        }
+
+        else if(btnText=="Help")
+        {
+            setDiscover(false);
+            setHelp(true);
+            setNotification(false);
+            setProfile(false);
+        }
+        else{
+            setDiscover(false);
+            setHelp(false);
+            setNotification(false);
+            setProfile(false);
+        }
+
+  
+
+    };
+    const openNotification = () =>
+    {
+        if(!notification)
+        {
+            setDiscover(false);
+            setHelp(false);
+            setNotification(true);
+            setProfile(false);
+        }
+        else
+        {
+            setNotification(false);
+        }
+    };
+
+    const openProfile = () =>{
+        if(!profile)
+        {
+            setDiscover(false);
+            setHelp(false);
+            setNotification(true);
+            setProfile(true);
+        }
+        else{
+
+        }setProfile(false);
+    };
+
+    const openSideBar = () =>
+    {
+        if(!sideMenu)
+        {
+            setSideMenu(true);
+        }   
+        else{
+            setSideMenu(false);
+        }
+    };
+
+    return (
+        <div className={Style.navbar}>
+            <div className={Style.navbar_container}>
+                <div className={Style.navbar_container_left}>
+                    <div className={Style.logo}>
+                        <Image src={images.logo} alt = "nft maeketplace"
+                        width={100}
+                        height={100}/>
+                    </div>
+                    <div className={Style.navbar_container_left_box}>
+                        <div className={Style.navbar_container_left_input}>
+                            <input type="text" placeholder="Search NFT" />
+                            <BsSearch onClick={ () => {}} className={Style.search_icon}/>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div className={Style.navbar_container_right}>
+                    <div className= {Style.navbar_container_right_discover}>
+                        
+                       
+                        <p onClick={(e) => openMenu(e)}>Discover</p>
+                        {discover && 
+                        (
+                            <div className={Style.navbar_container_right_discover_box}>
+                            <Discover />
+                        </div>
+                        )}
+                        </div>
+                        {/*help center*/}
+                        <div className= {Style.navbar_container_right_helpCenter}>
+                            <p onClick={ (e) => openMenu(e)}> Help</p>
+                            {help && (
+                                <div className={Style.navbar_container_right_helpCenter_box}> 
+                                    <HelpCenter />
+                                </div>
+                            )}
+                        </div>
+
+                    <div className={Style.navbar_container_right_notify}>
+                        <MdCircleNotifications className={Style.notify} onClick={ () => openNotification() } />
+
+                        {notification && <Notification/>}
+                    </div>
+
+                    <div className={Style.navbar_container_right_button}>
+                        <Button btnText="Create" />
+                    </div>
+
+                    {/*USER PROFILE*/}
+                    <div className={Style.navbar_container_right_profile_box}>
+                        <div className={Style.navbar_container_right_profile}>
+                            <Image src={images.user} alt="User IMage "
+                             width={40}
+                             height={40} 
+                             onClick={()=> openProfile() }
+                             className={Style.navbar_container_right_profile}/>
+                          {profile && <Profile />}
+
+                        </div>
+                    </div>
+
+                 {/*SIDE BAR MENU FOR MOBILE SECTION*/}
+
+                 <div className={Style.navbar_container_right_menu}>
+                    <CgMenuRight className={Style.menuIcon} 
+                    onClick={() => openSideBar()}/>
+
+                 </div>
+                </div>
+            </div>
+            
+             {sideMenu && (
+                <div className={Style.SideBar}>
+                    <SideBar setSideMenu = {setSideMenu} />
+                </div>
+             )}
+        </div>
+    )
+    
+}
+
+export default NavBar;
---
+++ img/index.js
@@ -0,0 +1,22 @@
+import creatorbackground1 from "./creatorbackground-1.jpeg";
+import creatorbackground2 from "./creatorbackground-2.jpeg";
+import creatorbackground3 from "./creatorbackground-3.jpeg";
+import creatorbackground4 from "./creatorbackground-4.jpg";
+import creatorbackground5 from "./creatorbackground-5.jpg";
+import creatorbackground6 from "./creatorbackground6.jpg";
+import creatorbackground7 from "./creatorbackground7.jpg";
+import creatorbackground9 from "./creatorbackground-9.jpg";
+import creatorbackground11 from "./creatorbackground-11.jpg";
+import nft from "./nft.png";
+import nft1 from "./nft1.png";
+import nft2 from "./nft2.png";
+import nft3 from "./nft3.png";
+import nft4 from "./nft4.png";
+import nft5 from "./nft5.png";
+import nft6 from "./nft6.png";
+import logo from "./logo.png";
+import user from "./user.png";
+
+export default {
+    creatorbackground1,creatorbackground2,creatorbackground3,creatorbackground4,creatorbackground5,creatorbackground6,creatorbackground7,creatorbackground9,creatorbackground11,nft,nft1,nft2,nft3,nft4,nft5,nft6,logo,user
+};
